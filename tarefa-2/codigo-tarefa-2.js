5c2fc3c53dc9a1770c7b52241e8a6e45ad308aff
Your Name <you@example.com>
2022-05-25 19:31:59 -0300

    retornando ao codigo

--- tarefa-2/codigo-tarefa-2.js
+++ tarefa-2/codigo-tarefa-2.js
@@ -1,69 +1,69 @@
-let camisaPolo = {"nome": "camiseta", "valor": 100.00, "internacional": true};
-let camisetaRegata = {"nome": "perfume", "valor": 200.00, "internacional": true};
-let bermudaSimples = {"nome": "sandália", "valor": 120.00, "internacional": false};
+// let camisaPolo = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+// let camisetaRegata = {"nome": "perfume", "valor": 200.00, "internacional": true};
+// let bermudaSimples = {"nome": "sandália", "valor": 120.00, "internacional": false};
 
 
-if (camisaPolo.internacional == true) {
-    //executa se for verdadeiro
-    let preco = camisaPolo.valor
-    //20% == 0.2
-    let imposto = 0.2
+// if (camisaPolo.internacional == true) {
+//     //executa se for verdadeiro
+//     let preco = camisaPolo.valor
+//     //20% == 0.2
+//     let imposto = 0.2
     
-    let valorComImposto = preco + preco * imposto
+//     let valorComImposto = preco + preco * imposto
 
-    console.log("O produto é internacional")
-    console.log("O valor com impostos é: ",valorComImposto)
+//     console.log("O produto é internacional")
+//     console.log("O valor com impostos é: ",valorComImposto)
 
-}else {
-    //executa se for falso
-    let preco = camisaPolo.valor
-    //12% == 0.12
-    let imposto = 0.12
+// }else {
+//     //executa se for falso
+//     let preco = camisaPolo.valor
+//     //12% == 0.12
+//     let imposto = 0.12
     
-    let valorComImposto = preco + preco * imposto
-    console.log("O produto é nacional")
-}   
+//     let valorComImposto = preco + preco * imposto
+//     console.log("O produto é nacional")
+// }   
 
-if (camisetaRegata.internacional == true) {
-    //executa se for verdadeiro
-    let preco = camisetaRegata.valor
-    //20% == 0.2
-    let imposto = 0.2
+// if (camisetaRegata.internacional == true) {
+//     //executa se for verdadeiro
+//     let preco = camisetaRegata.valor
+//     //20% == 0.2
+//     let imposto = 0.2
 
-    let valorComImposto = preco + preco * imposto
+//     let valorComImposto = preco + preco * imposto
 
-    console.log("O produto é internacional")
-    console.log("O valor com imposto é: ",valorComImposto)
+//     console.log("O produto é internacional")
+//     console.log("O valor com imposto é: ",valorComImposto)
 
-}else {
-    //executa se for falso
-    console.log("0 produto é nacional")
-    console.log("O valor com imposto é: ", camisetaRegata.valor + camisetaRegata.valor * 0.12)
-}
+// }else {
+//     //executa se for falso
+//     console.log("0 produto é nacional")
+//     console.log("O valor com imposto é: ", camisetaRegata.valor + camisetaRegata.valor * 0.12)
+// }
 
-if (bermudaSimples.internacional == true) {
-    //executa se for verdadeiro
-    let preco = bermudaSimples.valor
-    //20% == 0.2
-    let imposto = 0.2
+// if (bermudaSimples.internacional == true) {
+//     //executa se for verdadeiro
+//     let preco = bermudaSimples.valor
+//     //20% == 0.2
+//     let imposto = 0.2
     
-    let valorComImposto = preco + preco * imposto
+//     let valorComImposto = preco + preco * imposto
 
-    console.log("O produto é internacional")
-    console.log("O valor com impostos é: ",valorComImposto)
+//     console.log("O produto é internacional")
+//     console.log("O valor com impostos é: ",valorComImposto)
 
-}else {
-    //executa se for falso
-    let preco = bermudaSimples.valor
-    //12% == 0.12
-    let imposto = 0.12
+// }else {
+//     //executa se for falso
+//     let preco = bermudaSimples.valor
+//     //12% == 0.12
+//     let imposto = 0.12
     
-    let valorComImposto = preco + preco * imposto
+//     let valorComImposto = preco + preco * imposto
 
-    console.log("O produto é nacional")
-    console.log("O valor com imposto é: ",valorComImposto)
+//     console.log("O produto é nacional")
+//     console.log("O valor com imposto é: ",valorComImposto)
 
-}
+
 
 // FUNÇÕES - COMEÇO
 // declarando a função
@@ -81,26 +81,23 @@ if (bermudaSimples.internacional == true) {
 //     console.log("O segundo número é: ", y)
 //     console.log("A soma é: ", x + y)
 
-// }
-
-// somar(50,80)
-// somar(700, 1000)
+// cd codigo
 
 //INTERAÇÃO (LOOP)
-// let família = [
-//    {'nome': 'Otacílio' 'Pai', true,  'idade': 70}
-//    {'nome': 'cida' 'mãe': true, 'idade': 68}
-//    {'nome': 'Edson' 'filho': true, 'idade': 50}
-//    {'nome': 'Elaine' 'filha': true, 'idade': 43}
-//    {'nome': 'Otacílio' 'Filho': true, 'idade': 37}
-//    {'nome': 'Ellen' 'filha': true, 'idade': 35}
-//    {'nome': 'Mario' 'filho': false, 'idade' 36}
-// ]
-// console.log(família)
-// for (let i = 0; i < 7; i++) {
+let família = [
+   {'nome': 'Otacílio' 'Pai', true,  'idade': 70}
+   {'nome': 'cida' 'mãe': true, 'idade': 68}
+   {'nome': 'Edson' 'filho': true, 'idade': 50}
+   {'nome': 'Elaine' 'filha': true, 'idade': 43}
+   {'nome': 'Otacílio' 'Filho': true, 'idade': 37}
+   {'nome': 'Ellen' 'filha': true, 'idade': 35}
+   {'nome': 'Mario' 'filho': false, 'idade' 36}
+]
+console.log(família)
+for (let i = 0; i < 7; i++) {
 // o código que estiver aqui será repetido até o contador obedecer a condição
 // será repetido até 1 < 9, nesse caso.
-// console.log(família[i].nome)
+console.log(família[i].nome)
 
 
 
