5aba8fd81ee70baad2105c41673d9b7c958f7532
Your Name <you@example.com>
2022-05-26 17:32:32 -0300

    Continuando com meu conhecimento

---
+++ criando-codigo/script.js
@@ -0,0 +1,37 @@
+console.log("Melissa")
+
+function mensagemDeConsole () {
+    console.log("Olá Otacilio. Estamos programando.")
+}
+
+function mensagemParaUsuario () {
+    alert("Olá Otacilio. Estamos programando.")
+    }
+
+    //DOM - document Object Model
+
+    /* objetos
+
+    let objeto = {
+        "nome": "Otacilio Cunha"
+        "idade": 37
+        }
+        
+        objeto.nome
+
+        */
+        function usandoODOM () {
+        console.log(document)
+        }
+
+// RAPIDINHO
+let objeto = {
+    "nome": "Otacílio"
+    "idade"
+    "lista": [1,2,3,4,5]
+    "objeto1": {
+        "objeto2": "Ola, eu sou o objeto 2"
+    },
+}
+
+console.log(objeto.mensagem[])
